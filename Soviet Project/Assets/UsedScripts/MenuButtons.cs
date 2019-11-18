4e15707957a8747411969603911418db2b8d50ee
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-18 12:03:56 +1100

    added basic menus

---
+++ Soviet Project/Assets/UsedScripts/MenuButtons.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject _pauseMenu;
+    public GameObject _settingsMenu;
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+    public void OpenSettings()
+    {
+        if(Setting.SettingsOpen)
+        {
+            Setting.SettingsOpen = false;
+            _settingsMenu.SetActive(false);
+            _pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Setting.SettingsOpen = true;
+            _settingsMenu.SetActive(true);
+            _pauseMenu.SetActive(false);
+            Time.timeScale = 0;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1;
+            _pauseMenu.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked; // lock the mouse cursor
+            Cursor.visible = false;
+            isPaused = false;
+            return;
+
+        }
+        else
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None; // lock the mouse cursor
+            Cursor.visible = true;
+            _pauseMenu.SetActive(true);
+            isPaused = true;
+            return;
+        }
+    }
+}
---
+++ Soviet Project/Assets/UsedScripts/PlayerHandler.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerHandler : MonoBehaviour
+{
+    
+    [Header("Value Variables")]
+    public float maxHealth;
+    
+    public float curHealth, healRate;
+   
+    
+    
+    public Slider healthBar;
+   
+    [Header("Damage Effect Variables")]
+    
+    public GameObject deathImage;
+    
+    
+   
+   
+    public static bool isDead = false;
+  
+    bool canHeal;
+    float healTimer;
+   
+   
+   
+    public string character;
+    public Text text;
+    [Header("Check Point")]
+    public Transform curCheckPoint;
+
+    //[Header("Save")]
+    //public PlayerSaveAndLoad saveAndLoad;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+        healRate = 0;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (curHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+        if (healthBar.value != Mathf.Clamp01(curHealth/maxHealth))
+        {
+            LoseHealth();
+        }
+       
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            
+            curHealth -= 5;
+        }
+        
+        
+        
+       
+        if (!canHeal && curHealth < maxHealth && curHealth > 0)
+        {
+            healTimer += Time.deltaTime;
+            if (healTimer >= 5)
+            {
+                canHeal = true;
+            }
+        }
+    }
+    void LoseHealth()
+    {
+        curHealth = Mathf.Clamp(curHealth, 0, maxHealth);
+        healthBar.value = Mathf.Clamp01(curHealth / maxHealth);
+
+    }
+   
+    
+    void Death()
+    {
+        
+        isDead = true;
+        text.text = "";
+        deathImage.SetActive(true);
+
+
+        Invoke("DeathText", 2f);
+        Invoke("ReviveText", 6f);
+        Invoke("Revive", 9f);
+    }
+    private void LateUpdate()
+    {
+        if (canHeal && curHealth < maxHealth && curHealth > 0)
+        {
+            HealOverTime();
+        }
+
+    }
+    void Revive()
+    {
+        text.text = "";
+        isDead = false;
+        curHealth = maxHealth;
+       
+       
+        this.transform.position = curCheckPoint.position;
+        this.transform.rotation = curCheckPoint.rotation;
+        deathImage.SetActive(false);
+        
+    }
+    void DeathText()
+    {
+        text.text = "Well you've fucked up.";
+    }
+    void ReviveText()
+    {
+        text.text = "But I'll give you another chance";
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("CheckPoint"))
+        {
+            curCheckPoint = other.transform;
+           // saveAndLoad.Save();
+            healRate = 10;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("CheckPoint"))
+        {
+            healRate = 0;
+        }
+    }
+    public void DamagePlayer(float damage)
+    {
+        
+        
+        canHeal = false;
+        healTimer = 0;
+    }
+
+    public void HealOverTime()
+    {
+        if (curHealth > 0 && curHealth <= maxHealth && canHeal)
+        {
+            curHealth += Time.deltaTime * (healRate);
+        }
+    }
+}
