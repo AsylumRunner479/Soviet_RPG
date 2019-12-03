6fff63405cdd501a3b5bd36f9fca8da211072e83
MZKing06 <57853071+MZKing06@users.noreply.github.com>
2019-12-03 16:02:08 +1100

    created script plant selection manager
    created script called plant selection manager. this will be the selection screen, each cube for now is a game object and will be changed via buttons.

---
+++ Soviet Project/Assets/UsedScripts/Plant_Selector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plant_Selector : MonoBehaviour
+{
+    //plants as GameObjects
+   public GameObject Plant1;
+   public GameObject Plant2;
+   public GameObject Plant3;
+
+
+   private Vector3 PlantPosition;
+   private Vector3 Offscreenplant;
+
+
+    private void Awake()
+    {
+        PlantPosition = Plant1.transform.position;
+        Offscreenplant = Plant2.transform.position;
+    }
+
+    public void NextPlant()
+    {
+        
+
+    }
+
+    public void PreviousPlant()
+    {
+
+
+    }
+
+}
