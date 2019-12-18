60ba11db4bbd40245b5f393220debc95458fcca8
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-19 09:58:45 +1100

    added last names and started on advantages

--- Soviet Project/Assets/UsedScripts/NPCStats.cs
+++ Soviet Project/Assets/UsedScripts/NPCStats.cs
@@ -7,13 +7,18 @@ public class NPCStats : MonoBehaviour
 {
     public RandomNumber random;
     public int Charisma, Compassion, Tenacity, Greed, Cunning, Intuition, Hostility;
-    public Slider charisma, compassion, tenacity, greed, cunning, intuition, hostility;
+    private Slider charisma, compassion, tenacity, greed, cunning, intuition, hostility;
     public Text Name, displayname;
+    public Text Advantage, displayadvantage;
+    public Text Advantage1, displayadvantage1;
+    public Text Advantage2, displayadvantage2;
     public GameObject StatDisplay;
     public GameObject NPCDisplay;
     public Button Drink, Bribe, Promote, Rumour, Share, Execute;
     
-    private string[] names = new string[] { "Albert", "Bogdan", "Dmitry", "Eduard", "Fedir", "German", "Igor", "Julij", "Konstantin", "Lavrentii", "Maxim", "Naum", "Osip", "Peter", "Roman", "Tit", "Vlad", "Yuri", "Vyacheslav", "Goga", "Alexei", "Viktor", "Misha", "Innokentiy", "Stefan", "Stanislav", "Foma", "Ruslan", "Taras", "Mitrofan", "Erik", "Spartak", "Modest", "Garry", "Nikita", "Boris", "Dobrushin", "Trofim", "Anton", "Mikhail", "Abram", "Gedeon", "Dorofey", "Savin", "Isodor", "Leonid", "Gleb", "Valentin", "David", "Daniil", "Ippolit", "Kirill", "Lazar", "Filipp", "Marlen", "Nestor", "Robert", "Arkady", "Pasha", "Valeriy", "Rolan", "Makariy", "Yulian", "Gennady", "Sergei", "Fanasiy", "Khan", "Semyon", "Yakov", "Rurik", "Faddei", "Yefim", "Nikolay", "Sasha", "Vladimir", "Alexander", "Stas", "Ipatiy", "Kolmogorov", "Vitaliy", "Vladislav" };
+    private string[] names = new string[] { "Albert", "Bogdan", "Dmitry", "Eduard", "Fedir", "German", "Igor", "Julij", "Konstantin", "Lavrentii", "Maxim", "Naum", "Osip", "Peter", "Roman", "Tit", "Vlad", "Yuri", "Vyacheslav", "Goga", "Alexei", "Viktor", "Misha", "Innokentiy", "Stefan", "Stanislav", "Foma", "Ruslan", "Taras", "Mitrofan", "Erik", "Spartak", "Modest", "Garry", "Nikita", "Boris", "Dobrushin", "Trofim", "Anton", "Mikhail", "Abram", "Gedeon", "Dorofey", "Savin", "Isodor", "Leonid", "Gleb", "Valentin", "David", "Daniil", "Ippolit", "Kirill", "Lazar", "Filipp", "Marlen", "Nestor", "Robert", "Arkady", "Pasha", "Valeriy", "Rolan", "Makariy", "Yulian", "Gennady", "Sergei", "Fanasiy", "Khan", "Semyon", "Yakov", "Rurik", "Faddei", "Yefim", "Nikolay", "Sasha", "Vladimir", "Alexander", "Stas", "Ipatiy", "Kolmogorov", "Vitaliy", "Vladislav", "Artyom", "Yana", "Olga", "Fedor", "Vasily", "Ivanovich", "Leo", "Pierre", "Anna", "Natasha", "Andrei" };
+    private string[] advantage = new string[] {"Charming", "Nice", "Tough", "Corrupt", "Smart", "Perceptive", "Hateful", "Ugly", "Arsehole", "Wimp", "Honest", "Moron", "Naive", "Lovely" };
+    private string[] lastNames = new string[] {"Isayev", "Tikhonov", "Chapayev", "Petka", "Anka", "Furmanov", "Budyonny", "Caesar", "Rzhevsky", "Tolstoy", "Bezukhov", "Blonsky", "Romanov", "Pavlovna", "Rostova", "Bolkonsky", "Kuragin", "Vidor", "Bondarchuk", "Levi", "Livinov", "Solomin", "Aksyonov", "Kalashnikov", "Gorbachev", "Smirnov", "Chernov", "Putin", "Brezhnev", "Pushkin", "Lipatov", "Khruschev", "Yeltsin", "Medvedev", "Anatolievych", "Vladimirovich" };
     public string GetRandomName()
     {
         return names[Random.Range(0, names.Length)];
@@ -62,7 +67,10 @@ public class NPCStats : MonoBehaviour
         Intuition = random.number100;
         random.Random10();
         Hostility = random.number100;
-        Name.text = names[Random.Range(0, names.Length)];
+        Name.text = names[Random.Range(0, names.Length)] + "" + lastNames[Random.Range(0, lastNames.Length)];
+        Advantage.text = advantage[Random.Range(0, advantage.Length)];
+        Advantage1.text = advantage[Random.Range(0, advantage.Length)];
+        Advantage2.text = advantage[Random.Range(0, advantage.Length)];
     }
 
     // Update is called once per frame
@@ -84,6 +92,9 @@ public class NPCStats : MonoBehaviour
             intuition.value = Intuition;
             hostility.value = Hostility;
             displayname.text = Name.text;
+            displayadvantage.text = Advantage.text;
+            displayadvantage1.text = Advantage1.text;
+            displayadvantage2.text = Advantage2.text;
         }
         if (Display == false)
         {
