17c500f9871d6b6271c4179eb7558165ec05cea6
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-12-19 18:33:09 +1100

    changed stats based on advantages

--- Soviet Project/Assets/UsedScripts/NPCStats.cs
+++ Soviet Project/Assets/UsedScripts/NPCStats.cs
@@ -9,16 +9,16 @@ public class NPCStats : MonoBehaviour
     public int Charisma, Compassion, Tenacity, Greed, Cunning, Intuition, Hostility;
     private Slider charisma, compassion, tenacity, greed, cunning, intuition, hostility;
     public Text Name, displayname;
-    public Text Advantage, displayadvantage;
-    public Text Advantage1, displayadvantage1;
-    public Text Advantage2, displayadvantage2;
+    public string Advantage, displayadvantage;
+    //public Text Advantage1, displayadvantage1;
+    //public Text Advantage2, displayadvantage2;
     public GameObject StatDisplay;
     public GameObject NPCDisplay;
     public Button Drink, Bribe, Promote, Rumour, Share, Execute;
-    
+
     private string[] names = new string[] { "Albert", "Bogdan", "Dmitry", "Eduard", "Fedir", "German", "Igor", "Julij", "Konstantin", "Lavrentii", "Maxim", "Naum", "Osip", "Peter", "Roman", "Tit", "Vlad", "Yuri", "Vyacheslav", "Goga", "Alexei", "Viktor", "Misha", "Innokentiy", "Stefan", "Stanislav", "Foma", "Ruslan", "Taras", "Mitrofan", "Erik", "Spartak", "Modest", "Garry", "Nikita", "Boris", "Dobrushin", "Trofim", "Anton", "Mikhail", "Abram", "Gedeon", "Dorofey", "Savin", "Isodor", "Leonid", "Gleb", "Valentin", "David", "Daniil", "Ippolit", "Kirill", "Lazar", "Filipp", "Marlen", "Nestor", "Robert", "Arkady", "Pasha", "Valeriy", "Rolan", "Makariy", "Yulian", "Gennady", "Sergei", "Fanasiy", "Khan", "Semyon", "Yakov", "Rurik", "Faddei", "Yefim", "Nikolay", "Sasha", "Vladimir", "Alexander", "Stas", "Ipatiy", "Kolmogorov", "Vitaliy", "Vladislav", "Artyom", "Yana", "Olga", "Fedor", "Vasily", "Ivanovich", "Leo", "Pierre", "Anna", "Natasha", "Andrei" };
-    private string[] advantage = new string[] {"Charming", "Nice", "Tough", "Corrupt", "Smart", "Perceptive", "Hateful", "Ugly", "Arsehole", "Wimp", "Honest", "Moron", "Naive", "Lovely" };
-    private string[] lastNames = new string[] {"Isayev", "Tikhonov", "Chapayev", "Petka", "Anka", "Furmanov", "Budyonny", "Caesar", "Rzhevsky", "Tolstoy", "Bezukhov", "Blonsky", "Romanov", "Pavlovna", "Rostova", "Bolkonsky", "Kuragin", "Vidor", "Bondarchuk", "Levi", "Livinov", "Solomin", "Aksyonov", "Kalashnikov", "Gorbachev", "Smirnov", "Chernov", "Putin", "Brezhnev", "Pushkin", "Lipatov", "Khruschev", "Yeltsin", "Medvedev", "Anatolievych", "Vladimirovich" };
+    private string[] advantage = new string[] { "Charming", "Nice", "Tough", "Corrupt", "Smart", "Perceptive", "Hateful", "Ugly", "Arsehole", "Wimp", "Honest", "Moron", "Naive", "Lovely" };
+    private string[] lastNames = new string[] { "Isayev", "Tikhonov", "Chapayev", "Petka", "Anka", "Furmanov", "Budyonny", "Caesar", "Rzhevsky", "Tolstoy", "Bezukhov", "Blonsky", "Romanov", "Pavlovna", "Rostova", "Bolkonsky", "Kuragin", "Vidor", "Bondarchuk", "Levi", "Livinov", "Solomin", "Aksyonov", "Kalashnikov", "Gorbachev", "Smirnov", "Chernov", "Putin", "Brezhnev", "Pushkin", "Lipatov", "Khruschev", "Yeltsin", "Medvedev", "Anatolievych", "Vladimirovich" };
     public string GetRandomName()
     {
         return names[Random.Range(0, names.Length)];
@@ -29,6 +29,10 @@ public class NPCStats : MonoBehaviour
         CreateNPC();
         Debug.Log(GetRandomName());
     }
+    public void Fire()
+        {
+        CreateNPC();
+        }
     public void DrinkWith()
     {
         hostility.value -= 20;
@@ -68,9 +72,75 @@ public class NPCStats : MonoBehaviour
         random.Random10();
         Hostility = random.number100;
         Name.text = names[Random.Range(0, names.Length)] + "" + lastNames[Random.Range(0, lastNames.Length)];
-        Advantage.text = advantage[Random.Range(0, advantage.Length)];
-        Advantage1.text = advantage[Random.Range(0, advantage.Length)];
-        Advantage2.text = advantage[Random.Range(0, advantage.Length)];
+
+        for (int i = 0; i < 3; i++)
+        {
+            Advantage = advantage[Random.Range(0, advantage.Length)];
+            if (Advantage == "Charming")
+            {
+                Charisma += 30;
+            }
+            else if (Advantage == "Ugly")
+            {
+                Charisma -= 30;
+            }
+            else if (Advantage == "Nice")
+            {
+                Compassion += 30;
+            }
+            else if (Advantage == "Arsehole")
+            {
+                Compassion -= 30;
+            }
+            else if (Advantage == "Tough")
+            {
+                Tenacity += 30;
+            }
+            else if (Advantage == "Wimp")
+            {
+                Tenacity -= 30;
+            }
+            else if (Advantage == "Corrupt")
+            {
+                Greed += 30;
+            }
+            else if (Advantage == "Honest")
+            {
+                Greed -= 30;
+            }
+            else if (Advantage == "Smart")
+            {
+                Cunning += 30;
+            }
+            else if (Advantage == "Moron")
+            {
+                Cunning -= 30;
+            }
+            else if (Advantage == "Perceptive")
+            {
+                Intuition += 30;
+            }
+            else if (Advantage == "Naive")
+            {
+                Intuition -= 30;
+            }
+            else if (Advantage == "Hateful")
+            {
+                Hostility += 30;
+            }
+            else if (Advantage == "Lovely")
+            {
+                Hostility -= 30;
+            }
+            else
+            {
+
+            }
+
+        }
+        //Advantage.text = advantage[Random.Range(0, advantage.Length)];
+        //Advantage1.text = advantage[Random.Range(0, advantage.Length)];
+        //Advantage2.text = advantage[Random.Range(0, advantage.Length)];
     }
 
     // Update is called once per frame
